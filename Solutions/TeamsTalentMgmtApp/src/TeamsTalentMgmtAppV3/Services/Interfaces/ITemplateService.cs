cb300375f2c0d0f86d7ac893027c64410519bfdc
unknown <Lab User@CLIENT01.contoso.com>
2019-10-30 13:14:00 -0500

    first commit of lab files

---
+++ Solutions/TeamsTalentMgmtApp/src/TeamsTalentMgmtAppV3/Services/Interfaces/ITemplateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AdaptiveCards;
+using TeamTalentMgmtApp.Shared.Models.DatabaseContext;
+
+namespace TeamsTalentMgmtAppV3.Services.Interfaces
+{
+    public interface ITemplateService
+    {
+        AdaptiveCard GetAdaptiveCardForNewJobPosting(string description = null);
+        AdaptiveCard GetAdaptiveCardForInterviewRequest(Candidate candidate, DateTime interviewDate);
+    }
+}
